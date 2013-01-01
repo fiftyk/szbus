cb9c4fce7c907012128799d0166cbf91b60753a7
LiuRongtao <fiftyk@qq.com>
2013-01-01 23:35:45 +0800

    删除BusExtractor类

--- szbus.py
+++ szbus.py
@@ -1,8 +1,6 @@
 #coding=utf-8
-from pyquery import PyQuery as pq
 from base import Line,Stand
 import urllib,re,json
-from sync import Sync
 import socket
 socket.setdefaulttimeout(30)
 
@@ -16,96 +14,6 @@ def urlread(url,count=3):
         except:
             print "......连接超时，重试：%d次"%fails
             fails += 1
-
-class BusExtractor:
-    #根据站牌查询 > "途径线路","方向"，"更新时间","站距"
-    url01 = "http://www.szjt.gov.cn/apts/default.aspx?StandCode=%s"
-    #根据线路查询 > "站台","编号","车牌","进站时间"
-    url02 = "http://www.szjt.gov.cn/apts/APTSLine.aspx?LineGuid=%s"
-    #
-    selector01 = "#ctl00_MainContent_Message td"
-
-    #
-    pt01 = re.compile(r"LineGuid=(.*)&LineInfo=(.+)\((.+)\)")
-    pt02 = re.compile(r"StandName=(.+)$")
-
-    lines = {}#线路集合
-    stands = {}#站牌集合
-
-    sync = Sync("bus.sqlite")
-
-    @classmethod
-    def run(cls,code):
-        cls.get_lines_by_stand(code)
-
-    @classmethod
-    def get_lines_by_stand(cls,code):
-        '''
-        站点找线路
-        '''
-        #判断站点是否已经搜索过
-        if code in cls.stands:
-            print u"站点[%s]已经完成线路搜索"%code
-            return
-        else:
-            print u"开始站点[%s]的搜索..."%code
-            cls.stands.update({code:None})
-
-        page = pq(cls.url01%code)
-
-        result,size = [],0
-        for td in page(cls.selector01).items('td'):
-            href = td("a").attr("href")
-            if href is not None:
-                result.append(cls.pt01.findall(href)[0])
-            else:
-                result.append(td.html())
-            size += 1
-
-        lines = []
-        for i in xrange(size/5):
-            line = result[i*5:i*5+5]
-            lines.append(line)
-            
-            #插入数据库
-            cls.sync.append_line(*line[0])
-
-            #线路查询站点
-            cls.get_stands_by_line(line[0][0])
-
-    @classmethod
-    def get_stands_by_line(cls,guid):
-        '''
-        线路找站点
-        '''
-        #判断站点是否已经搜索过
-        if guid in cls.lines:
-            print u">>>线路[%s]已经完成线路搜索"%guid
-            return
-        else:
-            print u">>>开始线路[%s]的搜索..."%guid
-            cls.lines.update({guid:None})
-
-        page = pq(cls.url02%guid)
-
-        result,size = [],0
-        for td in page(cls.selector01).items('td'):
-            href = td("a").attr("href")
-            if href is not None:
-                result.append(cls.pt02.findall(href)[0])
-            else:
-                result.append(td.html())
-            size += 1
-
-        stands = []
-        for i in xrange(size/4):
-            stand = result[i*4:i*4+4]
-            stands.append(stand)
-
-            cls.sync.append_stand(stand[1],stand[0])
-
-            #站点查询线路
-            cls.get_lines_by_stand(stand[1])
 
 class SzMapExtractor:
     linelist = "http://www.sz-map.com/maps/bll?m=gllb"
@@ -159,7 +67,7 @@ if __name__ == '__main__':
         values = ",".join(["'%s'"%info for info in lineinfo.values()])
         # fp_lines.write(values.encode("utf-8")+"\n")
         print ",".join(lineinfo.keys())
-        
+
         for sta in stations:
             uid = sta.get("id")
             info = SzMapExtractor.get_stand_info(uid)
